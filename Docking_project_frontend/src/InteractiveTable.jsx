4b14290c83de0ae600819a833126f545a406d97c
Rousselene Jones <roussie@Rousselenes-MacBook-Pro.local>
2023-07-05 22:11:14 -0600

    fixed /Results route

--- Docking_project_frontend/src/InteractiveTable.jsx
+++ Docking_project_frontend/src/InteractiveTable.jsx
@@ -69,7 +69,7 @@ const Interactive = (props) => {
           .then((res) => {
             console.log(res.data);
             setInteractiveTableData(res.data);
-            navigate("https://bioinfo.usu.edu/myDockDB/results");
+            navigate("/Results");
           });
       });
     }
@@ -93,7 +93,7 @@ const Interactive = (props) => {
           .then((res) => {
             console.log(res.data);
             setInteractiveTableData(res.data);
-            navigate("https://bioinfo.usu.edu/myDockDB/results");
+            navigate("/Results");
           });
       });
     }
